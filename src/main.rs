cd4be83356ddad8f7a3a9eefd6f162487792243f
Julia Pitts <julia.pitts@outlook.com>
2022-05-18 16:54:17 -0700

    Routing might work now.

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,100 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
 
-enum Msg {
-    AddOne,
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/")]
+    Home,
+
+    #[at("/characters")]
+    Characters,
+    #[at("/magic")]
+    Magic,
+    #[at("/monsters")]
+    Monsters,
+    #[at("/npcs")]
+    NPCs,
+    #[at("/items")]
+    Items,
+    #[at("/cities")]
+    Cities,
+    #[at("/wild")]
+    TheWild,
+    #[at("/maze")]
+    TheMaze,
+
+    #[not_found]
+    #[at("/404")]
+    NotFound,
 }
 
-struct Model {
-    value: i64,
+fn switch(routes: &Route) -> Html {
+    match routes {
+        Route::Home => html! {
+            <>
+                <h2 class="title">{"Welcome!"}</h2>
+                <p>
+                    {"
+                    Welcome to the unofficial Maze Rats generator! Maze Rats (by Ben Milton/Questing
+                    Beast Games) has a lot of really wonderful random tables in it, and I thought that
+                    making a generator that rolls on them would be a fun day project.
+                    "}
+                </p>
+            </>
+        },
+
+        // Route::Characters => html! { <Characters /> },
+        // Route::Magic => html! { <Magic /> },
+        // Route::Monsters => html! { <Monsters /> },
+        // Route::NPCs => html! { <NPCs /> },
+        // Route::Items => html! { <Items /> },
+        // Route::Cities => html! { <Cities /> },
+        // Route::TheWild => html! { <TheWild /> },
+        // Route::TheMaze => html! { <TheMaze /> },
+        Route::NotFound | _ => html! { <h1>{ "404" }</h1> },
+    }
 }
 
-impl Component for Model {
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self { value: 0 }
-    }
-
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
-                true
-            }
-        }
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
-        let link = ctx.link();
-        html! {
+#[function_component(Main)]
+fn app() -> Html {
+    html! {
+        <BrowserRouter>
+            <nav class="navbar is-info has-shadow" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                    <p class="navbar-item title" style="margin-bottom: 0px;">
+                        {"Maze Rats Generator (unofficial)"}
+                    </p>
+                </div>
+            </nav>
             <section class="section">
-                <nav class="navbar" role="navigation" aria-label="main navigation">
-                    <div class="navbar-brand">
-                        <p class="navbar-item title">
-                            {"Maze Rats Generator (unofficial)"}
-                        </p>
-
-                        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                            <span aria-hidden="true"></span>
-                            <span aria-hidden="true"></span>
-                            <span aria-hidden="true"></span>
-                        </a>
-                    </div>
-                </nav>
                 <div class="container">
                     <div class="columns">
                         <div class="column is-narrow">
-                            <aside class="menu">
-                                <p class="subtitle has-text-centered">{"Maze Rats Generator"}</p>
+                            <aside class="menu box">
+                                <p class="menu-label">{"Maze Rats Generator"}</p>
                                 <ul class="menu-list">
-                                    <li><a>{"Characters"}</a></li>
-                                    <li><a>{"Magic"}</a></li>
-                                    <li><a>{"Monsters"}</a></li>
-                                    <li><a>{"NPCs"}</a></li>
-                                    <li><a>{"Items"}</a></li>
-                                    <li><a>{"Cities"}</a></li>
-                                    <li><a>{"The Wild"}</a></li>
-                                    <li><a>{"The Maze"}</a></li>
+                                    <li><Link<Route> to={Route::Characters}>{"Characters"}</Link<Route>></li>
+                                    <li><Link<Route> to={Route::Magic}>{"Magic"}</Link<Route>></li>
+                                    <li><Link<Route> to={Route::Monsters}>{"Monsters"}</Link<Route>></li>
+                                    <li><Link<Route> to={Route::NPCs}>{"NPCs"}</Link<Route>></li>
+                                    <li><Link<Route> to={Route::Items}>{"Items"}</Link<Route>></li>
+                                    <li><Link<Route> to={Route::Cities}>{"Cities"}</Link<Route>></li>
+                                    <li><Link<Route> to={Route::TheWild}>{"The Wild"}</Link<Route>></li>
+                                    <li><Link<Route> to={Route::TheMaze}>{"The Maze"}</Link<Route>></li>
                                 </ul>
                             </aside>
                         </div>
                         <div class="column">
-                            <h1 class="title">{"Section"}</h1>
-                            <h2 class="subtitle">
-                                {"A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading."}
-                            </h2>
-                            <div>
-                                <button onclick={link.callback(|_| Msg::AddOne)}>{"+1"}</button>
-                                <p>{self.value}</p>
-                            </div>
+                            <Switch<Route> render={Switch::render(switch)} />
                         </div>
                     </div>
                 </div>
             </section>
-        }
+        </BrowserRouter>
     }
 }
 
 fn main() {
-    yew::start_app::<Model>();
+    wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
+    yew::start_app::<Main>();
 }
